fe645f225798fb8f298e0c9a6bea9a6f3b8ec4db
Cederic Martinez <martinezcederic@gmail.com>
2020-03-18 13:07:03 +0800

    as

--- application/modules/portal/views/main/product_variants_view.php
+++ application/modules/portal/views/main/product_variants_view.php
@@ -785,6 +785,7 @@ $('.actionDone').on('click', function(){
                     $("#best_price").val(data.product_variants.best_price);
                     $("#old_price").val(data.product_variants.best_price);
                     $("#location").val(data.product_variants.location);
+                    $("#count").val(data.product_variants.count);
                     $("#inputProduct_variantsEmailAddress").val(data.product_variants.email_address);
 
                     $("#coverImgPrev").show();
@@ -855,6 +856,7 @@ $('.actionDone').on('click', function(){
                     $("#saveProduct_variants").html("Save Product").hide();
                     $("#add_color").attr("disabled","disabled");
                     $("#edit_image").attr("disabled","disabled");
+                    $("#count").val(data.product_variants.count);
 
                 },
                 error: function (request, status, error) {
