48a8c939a014ccac6bb8b1e97c40ed5a57b563b2
unknown <albarillocalvinjoe@gmail.com>
2020-10-27 18:13:00 +0800

    cmb formula for product category

--- application/modules/portal/controllers/Products.php
+++ application/modules/portal/controllers/Products.php
@@ -5,9 +5,9 @@ class Products extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->settings_model->get_settings();   
-        $this->load->model("portal/products_model"); 
-        
+        $this->settings_model->get_settings();
+        $this->load->model("portal/products_model");
+
         if($this->session->userdata("USERID") == null)
         {
                 echo "Sorry you are not logged in";
@@ -38,7 +38,7 @@ class Products extends CI_Controller {
         //$this->products_model->location = $this->input->post("location");
 
         echo $this->products_model->insert_products();
-        
+
 	}
 
 	public function edit_products()
@@ -71,10 +71,10 @@ class Products extends CI_Controller {
 	{
         $id = $this->input->post("id");
         $this->db->where("id",$id);
-        
+
         $data_products = $this->db->get("products");
         $this->db->where("id",$id);
-        $data["status"] = 3; 
+        $data["status"] = 3;
         echo $result = $this->db->update("products",$data);
         unlink($upload_path = './uploads/products/'.$data_products->row()->cover_image);
         $data = json_encode($data_products->row());
@@ -83,7 +83,7 @@ class Products extends CI_Controller {
         $this->logs->module = "products";
         $this->logs->created_by = $this->session->userdata("USERID");
         $this->logs->insert_log();
-        
+
 	}
 
     public function get_products_data()
@@ -101,22 +101,22 @@ class Products extends CI_Controller {
             }
         }
         $return["products"] = $products;
-        echo json_encode($return); 
+        echo json_encode($return);
     }
     public function get_products_selection()
     {
-        
+
         $search = $this->input->get("term[term]");
-        $this->db->like("title",$search);  
+        $this->db->like("title",$search);
         //$this->db->where("status",1);
-        $this->db->or_like("code",$search); 
-        //$this->db->where("status",1); 
-        $this->db->or_like("class",$search);  
+        $this->db->or_like("code",$search);
         //$this->db->where("status",1);
-        $this->db->select("title as text"); 
-        $this->db->select("class"); 
-        $this->db->select("code"); 
-        $this->db->select("description"); 
+        $this->db->or_like("class",$search);
+        //$this->db->where("status",1);
+        $this->db->select("title as text");
+        $this->db->select("class");
+        $this->db->select("code");
+        $this->db->select("description");
         $this->db->select("id");
         $this->db->limit(10);
         $filteredValues=$this->db->get("products")->result_array();
@@ -127,27 +127,27 @@ class Products extends CI_Controller {
     }
     public function get_products_list()
     {
-        $this->load->model("portal/data_table_model","dt_model");  
-        
-        $this->dt_model->select_columns = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status","(SELECT COUNT(*) FROM STOCKS WHERE status = 0 and product_variant_id in(SELECT id from product_variants WHERE product_id = t1.id   and product_variants.status = 1)) as quantity");  
+        $this->load->model("portal/data_table_model","dt_model");
+
+        $this->dt_model->select_columns = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status","(SELECT COUNT(*) FROM STOCKS WHERE status = 0 and product_variant_id in(SELECT id from product_variants WHERE product_id = t1.id   and product_variants.status = 1)) as quantity");
         if($this->session->userdata("USERTYPE") ==1)
         {
-            $this->dt_model->select_columns = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status","(SELECT COUNT(*) FROM STOCKS WHERE status = 0 and  product_variant_id in(SELECT id from product_variants WHERE product_id = t1.id   and product_variants.status = 1)) as quantity","t1.date_created","t2.username as created_by","t1.date_modified","t3.username as modified_by");  
+            $this->dt_model->select_columns = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status","(SELECT COUNT(*) FROM STOCKS WHERE status = 0 and  product_variant_id in(SELECT id from product_variants WHERE product_id = t1.id   and product_variants.status = 1)) as quantity","t1.date_created","t2.username as created_by","t1.date_modified","t3.username as modified_by");
         }
-      
+
         $this->dt_model->where  = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status");
         if($this->session->userdata("USERTYPE") ==1)
         {
-            $this->dt_model->where  = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status","t1.inner_carton","t1.date_created","t2.username","t1.date_modified","t3.username");  
-        }  
-        
-        $select_columns = array("id","class","code","description","inner_carton","master_carton","weight_of_box","minimum_of_quantity","lowest_cost","fob","quantity","status");    
-        if($this->session->userdata("USERTYPE") ==1){
-            $select_columns = array("id","class","code","description","inner_carton","master_carton","weight_of_box","minimum_of_quantity","lowest_cost","fob","quantity","status","date_created","created_by","date_modified","modified_by");  
+            $this->dt_model->where  = array("t1.id","t1.class","t1.code","t1.description","t1.inner_carton","t1.master_carton","t1.weight_of_box","t1.minimum_of_quantity","t1.lowest_cost","t1.fob","t1.status","t1.inner_carton","t1.date_created","t2.username","t1.date_modified","t3.username");
         }
-        $this->dt_model->table = "products AS t1 LEFT JOIN user_accounts AS t2 ON t2.id = t1.created_by LEFT JOIN user_accounts AS t3 ON t3.id = t1.modified_by";  
+
+        $select_columns = array("id","class","code","description","inner_carton","master_carton","weight_of_box","minimum_of_quantity","lowest_cost","fob","quantity","status");
+        if($this->session->userdata("USERTYPE") ==1){
+            $select_columns = array("id","class","code","description","inner_carton","master_carton","weight_of_box","minimum_of_quantity","lowest_cost","fob","quantity","status","date_created","created_by","date_modified","modified_by");
+        }
+        $this->dt_model->table = "products AS t1 LEFT JOIN user_accounts AS t2 ON t2.id = t1.created_by LEFT JOIN user_accounts AS t3 ON t3.id = t1.modified_by";
         $this->dt_model->index_column = "t1.id";
-        $this->dt_model->staticWhere = "t1.status != 3"; 
+        $this->dt_model->staticWhere = "t1.status != 3";
         $result = $this->dt_model->get_table_list();
         $output = $result["output"];
         $rResult = $result["rResult"];
@@ -158,6 +158,18 @@ class Products extends CI_Controller {
                     if($col == "username" || $col == "created_by" || $col == "modified_by")
                     {
                         $row[] = $aRow[$col];
+                    }else if($col == "weight_of_box")
+                    {
+                      $res_mstr=0;
+                      $mstr_data=trim(strtolower($aRow['master_carton']));
+                      $slice_mstr=explode('x',$mstr_data);
+                      if(count($slice_mstr)>0){
+                        foreach ($slice_mstr as $value) {
+                          $res_mstr=($res_mstr<1)?$res_mstr+floatval(trim($value)):$res_mstr*floatval(trim($value));
+                        }
+                        $res_mstr=$res_mstr/61023;
+                      }
+                        $row[] = number_format($res_mstr,4);
                     }
                     else if($col == "fob")
                     {
@@ -169,7 +181,7 @@ class Products extends CI_Controller {
                         }else{
                             $row[] = $aRow[$col] ;
                         }
-                        
+
                     }
                     else if($col == "status")
                     {
@@ -187,7 +199,7 @@ class Products extends CI_Controller {
                     else if($col == "cover_image")
                     {
                         if($aRow[$col] != null)
-                        {    
+                        {
                             $row[] = "<center><a href=\"#\" onclick='return false;'><img class='img-thumbnail' src='".base_url()."uploads/products/".$aRow[$col]."' style='height:70px;' onclick='img_preview(\"".$aRow[$col]."\");return false;'></a></center>";
                         }
                         else
@@ -200,7 +212,7 @@ class Products extends CI_Controller {
                         $row[] = $aRow[$col] ;
                     }
             }
-            
+
             $btns = '<a href="#" onclick="_view('.$aRow['id'].');return false;" class="glyphicon glyphicon-search text-orange" data-toggle="tooltip" title="View Details"></a>
             <a href="#" onclick="_edit('.$aRow['id'].');return false;" class="glyphicon glyphicon-edit text-blue" data-toggle="tooltip" title="Edit"></a>
             <a href="#" onclick="_delete('.$aRow['id'].',\''.htmlentities($aRow["description"]).'\');return false;" class="glyphicon glyphicon-remove text-red" data-toggle="tooltip" title="Delete"></a>';
@@ -210,6 +222,6 @@ class Products extends CI_Controller {
         echo json_encode( $output );
     }
 
-    
+
 
 }
