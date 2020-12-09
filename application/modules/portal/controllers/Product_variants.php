4ebf1966993447a7cda17d71a9c065aa44eb2a1d
Cederic Martinez <33000508+akosicedzkii@users.noreply.github.com>
2020-12-09 17:27:03 +0800

    Update Product_variants.php

--- application/modules/portal/controllers/Product_variants.php
+++ application/modules/portal/controllers/Product_variants.php
@@ -167,10 +167,10 @@ class Product_variants extends CI_Controller {
         {
             $this->dt_model->where  = array("t1.id","t1.id","t4.class","t4.code","t4.description","t1.location","t1.color","t1.proto","t1.molds","t1.status","t1.date_created","t2.username","t1.date_modified","t3.username");  
         }
-        $select_columns = array("id","cover_image","class","code","description","location","color","proto","molds","stock","fob","status");  
+        $select_columns = array("id","cover_image","class","code","location","color","proto","molds","stock","fob","status");  
         if($this->session->userdata("USERTYPE") ==1)
         {
-            $select_columns = array("id","cover_image","class","code","description","location","color","proto","molds","stock","fob","status","date_created","created_by","date_modified","modified_by");
+            $select_columns = array("id","cover_image","class","code","location","color","proto","molds","stock","fob","status","date_created","created_by","date_modified","modified_by");
         }  
         $this->dt_model->table = "product_variants AS t1 LEFT JOIN user_accounts AS t2 ON t2.id = t1.created_by LEFT JOIN user_accounts AS t3 ON t3.id = t1.modified_by LEFT JOIN products AS t4 ON t4.id = t1.product_id"; 
         $this->dt_model->index_column = "t1.id"; 
